7db87493ba257cabc1e036fe2c742176ee2fefe7
Kuschel_Swein <kuschelswein@gmail.com>
2024-11-18 19:20:31 +0100

    Resolve #7

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import type { Theme } from 'vitepress';
 import EncryptedSolution from '../../src/components/EncryptedSolution.vue';
+import MaterialFAQ from '../../src/components/MaterialFAQ.vue';
 import Layout from './Layout.vue';
 import './style/style.scss';
 
@@ -7,6 +8,9 @@ export default {
     Layout,
     enhanceApp({ app }) {
         // make the commonly used components available everywhere
-        app.component('EncryptedSolution', EncryptedSolution);
+        app.component('EncryptedSolution', EncryptedSolution).component(
+            'MaterialFAQ',
+            MaterialFAQ,
+        );
     },
 } satisfies Theme;
---
+++ src/components/MaterialFAQ.vue
@@ -0,0 +1,34 @@
+<template>
+    <details>
+        <summary class="summary">
+            Ich kann die Aufgabe schon alleine lösen, bin ich jetzt vergebens
+            hier?
+        </summary>
+        Nein! Ihr könnt gerne aufpassen, dass ich keinen Fehler mache und ggf.
+        euren eigenen Code verbessern.
+    </details>
+
+    <details v-if="hasCommand">
+        <summary class="summary">Was macht der Befehl überhaupt?</summary>
+        Dieser Befehl lädt Code herunter und führt diesen direkt aus, sodass für
+        euch komfortabel ein weiteres ZIP-Archiv heruntergeladen und gleich
+        entpackt wird. All das wird anschließend wieder aufgeräumt.
+    </details>
+</template>
+
+<script lang="ts" setup>
+withDefaults(
+    defineProps<{
+        hasCommand: boolean;
+    }>(),
+    {
+        hasCommand: true,
+    },
+);
+</script>
+
+<style lang="css" scoped>
+.summary {
+    font-weight: bold;
+}
+</style>
