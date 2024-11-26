b8e917f78140dd642ca46368d34c6425b5d3d40c
Kuschel_Swein <kuschelswein@gmail.com>
2024-11-26 21:29:03 +0100

    Fix 404 page to work with new style

--- .vitepress/theme/components/NotFound.vue
+++ .vitepress/theme/components/NotFound.vue
@@ -1,27 +1,34 @@
 <template>
-    <div class="notice">
-        <div class="notice__icon">🤔</div>
-        <h1>Nicht gefunden</h1>
-        <p>Die angeforderte Seite konnte nicht gefunden werden.</p>
+    <div :class="$style.notice">
+        <div :class="$style.notice__icon">🤔</div>
+        <h1 :class="$style.notice__content">Nicht gefunden</h1>
+        <p :class="$style.notice__content">
+            Die angeforderte Seite konnte nicht gefunden werden.
+        </p>
     </div>
 </template>
 
-<style lang="css" scoped>
+<style lang="scss" module>
 .notice {
     display: flex;
     flex-direction: column;
 
     justify-content: center;
     align-items: center;
-    text-align: center;
-
-    padding: 1rem;
 
     width: 100vw;
     height: 100vh;
 
-    .notice__icon {
+    color: var(--fgColor-default);
+    text-align: center;
+
+    &__icon {
         font-size: 400%;
+    }
+
+    &__content {
+        padding-right: 1rem;
+        padding-left: 1rem;
     }
 }
 </style>
