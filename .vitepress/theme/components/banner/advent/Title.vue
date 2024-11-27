277e17223f392e332cd7c5f71b02a97df16fe02b
Kuschel_Swein <kuschelswein@gmail.com>
2024-11-27 14:45:36 +0100

    Use css modules for advent ad

--- .vitepress/theme/components/banner/advent/Title.vue
+++ .vitepress/theme/components/banner/advent/Title.vue
@@ -1,23 +1,23 @@
 <template>
-    <h2 class="title">
+    <h2 :class="$style.title">
         <span class="title__keyword--advent">Advent</span>
         <span class="title__keyword">&nbsp;of&nbsp;</span>
         <span class="title__keyword--inf">InfLabs</span>
     </h2>
 
-    <p class="title__subtitle">
+    <p :class="$style.title__subtitle">
         Löse jeden Adventssonntag eine kleine Aufgabe, um deine
         Programmierfähigkeiten zu verbessern!
     </p>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss" module>
 .title {
-    font-size: 2rem !important;
     margin: 0 !important;
     margin-top: 1rem !important;
-    font-weight: bold !important;
     border-bottom: none !important;
+    font-weight: bold !important;
+    font-size: 2rem !important;
 
     &__subtitle {
         margin: 0;
@@ -26,23 +26,23 @@
 
     &__keyword {
         &--advent {
-            color: #0000;
-            background-clip: text;
             background-image: linear-gradient(
                 to right,
                 rgb(22, 163, 74),
                 rgb(239, 68, 68)
             );
+            background-clip: text;
+            color: #0000;
         }
         &--inf {
-            color: #0000;
-            background-clip: text;
             background-image: linear-gradient(
                 to right,
                 rgb(234, 179, 8),
                 rgb(249, 115, 22),
                 rgb(185, 28, 28)
             );
+            background-clip: text;
+            color: #0000;
         }
     }
 }
