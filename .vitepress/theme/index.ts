310f28cdc2f96a667de793ea27f7da99250297be
Kuschel-Swein <kuschelswein@gmail.com>
2025-08-10 00:15:05 +0200

    Deprecate components

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 import type { Theme } from 'vitepress';
-import MaterialFAQ from '../../src/components/MaterialFAQ.vue';
-import EncryptedSolution from '../../src/components/solution/publish/EncryptedSolution.vue';
-import EncryptedSolutionGroup from '../../src/components/solution/publish/EncryptedSolutionGroup.vue';
+import EncryptedSolution from '../../src/components/_deprecated/EncryptedSolution.vue';
+import EncryptedSolutionGroup from '../../src/components/_deprecated/EncryptedSolutionGroup.vue';
+import MaterialFAQ from '../../src/components/_deprecated/MaterialFAQ.vue';
 import Layout from './Layout.vue';
 import './style/style.scss';
 
---
+++ src/components/_deprecated/MaterialFAQ.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script lang="ts" setup>
+withDefaults(
+    defineProps<{
+        hasCommand: boolean;
+    }>(),
+    {
+        hasCommand: true,
+    },
+);
+
+console.warn(`Using <MaterialFAQ> is deprecated, do not use material pages.`);
+</script>
